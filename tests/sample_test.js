f093b1b912175e93ba37fd04d578e07a7d09f91a
tkumark <t1279k@gmail.com>
2016-12-28 10:58:00 +0530

    changed to helloworld

--- tests/sample_test.js
+++ tests/sample_test.js
@@ -3,7 +3,7 @@ module.exports = {
     browser
       .url("http://127.0.0.1:5001")
       .waitForElementVisible('body', 1000)
-      .assert.containsText('body', 'Hello World')
+      .assert.containsText('body', 'Hello World1')
       .end();
   }
 };
